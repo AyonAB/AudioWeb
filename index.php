b7281fb7dc468fe86e6443ba9c2f39e390ea6aa4
Ayon Das <30872214+AyonAB@users.noreply.github.com>
2018-02-12 14:46:08 +0530

    Update index.php
    Complete Functionality of every button.
    Buttons styled with bootstrap.
    No layout or grid design yet!

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
-$dir = 'C:\xampp\htdocs\AudioWeb\Music';
+$dir = 'Music';
 $filenames = scandir($dir);
 $no_of_files = count($filenames);
 ?>
 <html>
 <head>
   <title>AudioWeb</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script>
       function play(element){
           var audio = document.getElementById(element);
@@ -27,17 +31,18 @@ $no_of_files = count($filenames);
          onlyPlayOneIn(document.body);
        });
        function universalPlay(){
+         var flag = 0;
          var audio = document.getElementsByTagName('audio');
          for(i=0;i<audio.length;i++){
-           if(audio[i].currentTime > 0){
+           if(audio[i].currentTime > 0 && audio[i].paused){
              audio[i].play();
-             break;
-           }
-           else{
-             audio[0].play();
+             flag = 1;
              break;
            }
          }
+           if(flag == 0){
+               audio[0].play();
+           }
        }
        function pauseAllAudio() {
          var sounds = document.getElementsByTagName('audio');
@@ -57,20 +62,26 @@ $no_of_files = count($filenames);
 <body>
   <h2 style = 'text-align:center'> Audio Web</h2>
   <div style = 'text-align:center'>
-    <input id="playButton" type="button" value="Play" onclick="universalPlay()"/>
-    <input id="pauseButton" type="button" value="Pause" onclick="pauseAllAudio()"/>
-    <input id="stopButton" type="button" value="Stop" onclick="stopAllAudio()"/><br><br>
+    <a href="#" class="btn btn-info btn-lg" onclick="universalPlay()">
+          <span class="glyphicon glyphicon-play"></span> Play
+    </a>
+    <a href="#" class="btn btn-info btn-lg" onclick="pauseAllAudio()">
+          <span class="glyphicon glyphicon-play"></span> Pause
+    </a>
+    <a href="#" class="btn btn-info btn-lg" onclick="stopAllAudio()">
+          <span class="glyphicon glyphicon-play"></span> Stop
+    </a><br><br>
   </div>
-  <div style = 'text-align:center'>
+  <div style = 'text-align:center' class = "row">
     <?php
         for($i=2,$n=1;$i<$no_of_files;$i++,$n++){
           ?>
-
-          <input type="button" value="<?php echo '#'.$n.' '.trim($filenames[$i],".mp3");?>"  onclick="play('<?php echo $filenames[$i];?>')"/>
-          <audio id="<?php echo $filenames[$i];?>" src="<?php echo $filenames[$i];?>" ></audio>
+      
+          <a href="#" class="btn btn-primary " onclick="play('<?php echo $filenames[$i];?>')"><span class="glyphicon glyphicon-music"></span> <?php echo '#'.$n.' '.trim($filenames[$i],".mp3");?></a>
+      
+          <audio id="<?php echo $filenames[$i];?>" src="<?php echo $dir.'\\'.$filenames[$i];?>" ></audio>
 
           <?php
-          //echo trim($filenames[$i],".mp3");
         }
      ?>
   </div>
